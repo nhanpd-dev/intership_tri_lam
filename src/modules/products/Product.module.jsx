788d4e7c3cc5f6f77908ee7f29852248957c2060
HoangLamLe <meoulam192@gmail.com>
2022-08-11 10:19:10 +0700

    create products list UI 1st commit #1

--- src/modules/products/Product.module.jsx
+++ src/modules/products/Product.module.jsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Image, Pagination } from 'antd';
+import { StarFilled } from '@ant-design/icons';
+
+import { Container, ProductItem, Products } from './styled';
+import { data } from './Products';
+
+const pageSize = 10;
 
 const ProductsModule = () => {
-  return <>PRODUCTS MODULE</>;
+  const [pagination, setPagination] = useState({
+    totalPage: data.length / pageSize,
+    current: 1,
+    minIndex: 0,
+  });
+
+  const { t } = useTranslation(['common']);
+
+  const handlePaginationChange = (page) => {
+    setPagination({
+      ...pagination,
+      current: page,
+      minIndex: page * pageSize - pageSize,
+    });
+  };
+
+  return (
+    <Container>
+      {data.length === 0 ? (
+        <div>Chưa có dữ liệu sản phẩm</div>
+      ) : (
+        <>
+          <Products>
+            {data.slice(pagination.minIndex, pagination.minIndex + pageSize).map((product) => (
+              <ProductItem key={product.id}>
+                <div className='thumbnail'>
+                  <Image preview={false} className='thumbnail-img' src={product.avatar} alt='productImage' />
+                </div>
+                <div className='title'>{product.name}</div>
+                <div className='info'>
+                  <div className='rating'>
+                    <StarFilled />
+                    <StarFilled />
+                    <StarFilled />
+                    <StarFilled />
+                    <StarFilled />
+                  </div>
+                  <div className='sold'>
+                    {t('sold')} {product.sold}
+                  </div>
+                </div>
+                <div className='price'>
+                  {product.price} {t('currency')}
+                </div>
+              </ProductItem>
+            ))}
+          </Products>
+          <Pagination
+            pageSize={pageSize}
+            current={pagination.current}
+            onChange={handlePaginationChange}
+            total={data.length}
+          />
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default ProductsModule;
