6f3ce2c9f50a6f3a470d1422fda94d15bc3dae0e
LamLee <admin@Mac-cua-Lam.local>
2022-08-17 09:33:26 +0700

    update/lam_Lqb/INTERSHIP_18_Update-Cart-part5

--- src/modules/cart/Cart.module.js
+++ src/modules/cart/Cart.module.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
-import { Col, Typography } from 'antd';
+import { Col, Row, Typography } from 'antd';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 import HaveProducts from './haveProducts/HaveProducts';
 import NoProducts from './noProducts/NoProducts';
@@ -17,7 +18,9 @@ function Cart() {
   return (
     <MyCart>
       <Col span={22} offset={1}>
-        <Title level={3}>{t('my_cart')}</Title>
+        <Row>
+          <Title level={3}>{t('my_cart')}</Title>
+        </Row>
       </Col>
       {cart ? <HaveProducts /> : <NoProducts />}
     </MyCart>
