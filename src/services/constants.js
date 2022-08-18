c89bbf3757fea6bfc701ee048607cdee8fc3f3cb
LamLee <admin@Mac-cua-Lam.local>
2022-08-18 16:18:49 +0700

    fix-conflict

--- src/services/constants.js
+++ src/services/constants.js
@@ -3,9 +3,6 @@ export const ENDPOINTS = {
   LOGIN: '/auth/login',
   GETME: '/me',
   UPDATE_USER: '/users/change-information',
-<<<<<<< HEAD
   UPDATE_USER_PASSWORD: '/users/change-password',
-=======
->>>>>>> 7ca6aec (update/lam_Lqb/INTERSHIP_18_Update-Cart-part4)
   ORDER: '/order',
 };
