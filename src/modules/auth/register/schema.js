4fd4d3f88f4da094a2e59a6fccdf89ac3a85b1e7
LamLee <admin@Mac-cua-Lam.local>
2022-08-19 08:17:14 +0700

    Fix bug register

--- src/modules/auth/register/schema.js
+++ src/modules/auth/register/schema.js
@@ -2,8 +2,8 @@ import * as yup from 'yup';
 
 export const schema = yup.object().shape({
   email: yup.string().required('enter_this_field').email('email_is_incorrect'),
-  password: yup.string().required('enter_this_field').min(8, 'password_min_lenght').max(16, 'password_max_lenght'),
-  full_name: yup.string().required('enter_this_field').min(6, 'fullName_min_lenght').max(30, 'fullName_max_lenght'),
+  password: yup.string().required('enter_this_field').min(8, 'password_min_length').max(16, 'password_max_length'),
+  full_name: yup.string().required('enter_this_field').min(6, 'fullName_min_length').max(30, 'fullName_max_length'),
   phone_number: yup
     .string()
     .required('enter_this_field')
