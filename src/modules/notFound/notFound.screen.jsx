ed362c7750ece98af1b6cec9148ba5be68ce46de
LamLee <admin@Mac-cua-Lam.local>
2022-08-19 14:10:51 +0700

    update page notFound

--- src/modules/notFound/notFound.screen.jsx
+++ src/modules/notFound/notFound.screen.jsx
@@ -1,7 +1,21 @@
-import React from "react"
+import React from 'react';
+import { Col, Image, Row } from 'antd';
+
+import { IMG_NOTFOUND } from '../../assets/imgs/notFound/index';
+import { Wrapper } from './styled';
 
 function NotFoundScreen() {
-  return <div>NotFound Screen</div>;
+  return (
+    <Wrapper>
+      <Row>
+        <Col md={22} offset={1}>
+          <Row>
+            <Image src={IMG_NOTFOUND} preview={false} />
+          </Row>
+        </Col>
+      </Row>
+    </Wrapper>
+  );
 }
 
 export default NotFoundScreen;
---
+++ src/modules/notFound/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  margin-top: 20px;
+  width: 100%;
+
+  .title_support {
+    margin-top: 20px;
+    justify-content: center;
+  }
+`;
